2c57fe3ad5e7a2b65504e1ee6c837a431e1d2408
errmac-v <tmbrlkV@users.noreply.github.com>
2020-04-04 21:51:58 +0300

    Initial commit

---
+++ src/core/status/mod.rs
@@ -0,0 +1,52 @@
+use chrono::SubsecRound;
+use std::{convert::TryInto, ops::Add, time::Duration};
+
+/// A report for a given key containing the limit status.
+///
+/// The status contains the following information:
+///
+/// - [`limit`]: the maximum number of requests allowed in the current period
+/// - [`remaining`]: how many requests are left in the current period
+/// - [`reset_epoch_utc`]: a UNIX timestamp in UTC approximately when the next period will begin
+#[derive(Clone, Debug)]
+pub struct Status {
+    pub(crate) limit: usize,
+    pub(crate) remaining: usize,
+    pub(crate) reset_epoch_utc: usize,
+}
+
+impl Status {
+    pub fn limit(&self) -> usize {
+        self.limit
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.remaining
+    }
+
+    pub fn reset_epoch_utc(&self) -> usize {
+        self.reset_epoch_utc
+    }
+
+    pub(crate) fn build_status(count: usize, limit: usize, reset_epoch_utc: usize) -> Self {
+        let remaining = if count >= limit { 0 } else { limit - count };
+
+        Status {
+            limit,
+            remaining,
+            reset_epoch_utc,
+        }
+    }
+
+    pub(crate) fn epoch_utc_plus(duration: Duration) -> Result<usize, time::OutOfRangeError> {
+        Ok(chrono::Utc::now()
+            .add(chrono::Duration::from_std(duration)?)
+            .round_subsecs(0)
+            .timestamp()
+            .try_into()
+            .unwrap_or(0))
+    }
+}
+
+#[cfg(test)]
+mod test;
